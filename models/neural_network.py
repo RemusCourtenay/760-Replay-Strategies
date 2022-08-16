326c9f0242df1e7a46fbf940da0d2034cd7ee4f4
Remus Courtenay <62123294+RemusCourtenay@users.noreply.github.com>
2022-08-17 11:23:35 +1200

    update to match train in default_neural_network

--- models/neural_network.py
+++ models/neural_network.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
+from data.data_set import DataSet
 
 
 class NeuralNetwork(ABC):
-
+    
+    # TODO... set default values for test_acc and epochs
     @abstractmethod
-    def train(self, training_data):
+    def train(self, training_data: DataSet, test_acc, epochs):
         # TODO... figure out what methods this abstract class needs
         pass

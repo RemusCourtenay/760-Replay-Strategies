6e63bea9a02f699dffed4d81a1e94f53b9f1ee94
Remus Courtenay <62123294+RemusCourtenay@users.noreply.github.com>
2022-08-17 11:27:55 +1200

    Update to use new classes

--- strategies/selection_strategy.py
+++ strategies/selection_strategy.py
@@ -1,11 +1,18 @@
 from abc import ABC, abstractmethod
+from model.neural_network import NeuralNetwork
+from data.data_set import DataSet
 
 
 class SelectionStrategy(ABC):
 
-    def __init__(self, model: NeuralNetwork):
+    def __init__(self, model: NeuralNetwork, data: DataSet):
         self.model = model
+        self.data = data
 
     @abstractmethod
-    def train_model(self, data: DataSet):
+    def train_model(self):
+        pass
+    
+    @abstractmethod
+    def select_memories(self):
         pass

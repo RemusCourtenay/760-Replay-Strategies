78cd6dc8231818e3e5b222c69647ad1c1694a0cf
Remus Courtenay <62123294+RemusCourtenay@users.noreply.github.com>
2022-08-17 11:31:40 +1200

    copied select memories from random select

--- strategies/default_selection_strategy.py
+++ strategies/default_selection_strategy.py
@@ -3,9 +3,27 @@ from strategies.selection_strategy import SelectionStrategy
 
 class DefaultSelectionStrategy(SelectionStrategy):
 
-    def __init__(self, model: NeuralNetwork):
-        super().__init__(model)
+    def __init__(self, model: NeuralNetwork, data: DataSet):
+        super().__init__(model, data)
 
-    def train_model(self, data: DataSet):
+    def train_model(self):
         # TODO...
         pass
+    
+    # TODO... clean this up
+    def select_memories(self):
+        data = self.data.training_data
+        label = self.data.training_data_labels
+        
+        n = len(data)
+        # create an array of indexes to shuffle
+        s = np.arange(0, n)
+        np.random.shuffle(s)
+        # shuffle the data and label arrays
+        data_subset = data[s]
+        label_subset = label[s]
+
+        # cut subset down to desired size
+        data_subset = data_subset[:int(n*percentage)]
+        label_subset = label_subset[:int(n*percentage)]
+        return data_subset, label_subset

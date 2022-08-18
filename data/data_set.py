d51ee00baf54fdcaf58a6039d5529304b33f3076
Remus Courtenay <rcou199@aucklanduni.ac.nz>
2022-08-19 09:54:40 +1200

    Added comments explaining methods

--- data/data_set.py
+++ data/data_set.py
@@ -5,28 +5,33 @@ class DataSet(ABC):
 
     @abstractmethod
     def update_training_set(self, selected_memory_data, selected_memory_labels):
+        """Updates the currently selected training set by choosing a new task/tasks to train on and then adding any
+        old data values that the selection policy decided to keep."""
         pass
 
     @abstractmethod
     def get_tasks(self):
+        """Returns the full set of all non-validation data-label tuples as a two dimensional array sorted by task."""
         pass
 
     @abstractmethod
     def get_training_set(self):
-        # Implementations should return the training set
+        """Returns the set of currently selected data objects that are either in the currently training task or were
+        selected by the selection policy"""
         pass
 
     @abstractmethod
     def get_training_labels(self):
-        # Implementations should return the training labels
+        """Returns the set of currently selected data labels that are either in the currently training task or were
+        selected by the selection policy"""
         pass
 
     @abstractmethod
     def get_validation_set(self):
-        # Implementations should return the validation set
+        """Returns the set of validation data objects that are used to ensure that the model isn't over-fitting"""
         pass
 
     @abstractmethod
     def get_validation_labels(self):
-        # Implementations should return the validation labels
+        """Returns the set of validation data labels that are used to ensure that the model isn't over-fitting"""
         pass
--- data/default_data_set.py
+++ data/default_data_set.py
@@ -14,25 +14,34 @@ class DefaultDataSet(DataSet):
         self.validation_data = None
 
     def update_training_set(self, selected_memory_data, selected_memory_labels):
+        """Updates the currently selected training set by choosing a new task/tasks to train on and then adding any
+        old data values that the selection policy decided to keep."""
         # TODO...
         pass
 
     def get_tasks(self):
+        """Returns the full set of all non-validation data-label tuples as a two dimensional array sorted by task."""
         # TODO...
         pass
 
     def get_training_set(self):
+        """Returns the set of currently selected data objects that are either in the currently training task or were
+        selected by the selection policy"""
         # TODO...
         pass
 
     def get_training_labels(self):
+        """Returns the set of currently selected data labels that are either in the currently training task or were
+        selected by the selection policy"""
         # TODO...
         pass
 
     def get_validation_set(self):
+        """Returns the set of validation data objects that are used to ensure that the model isn't over-fitting"""
         # TODO...
         pass
 
     def get_validation_labels(self):
+        """Returns the set of validation data labels that are used to ensure that the model isn't over-fitting"""
         # TODO...
         pass

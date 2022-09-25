6e001ec2a73b2c3d526c2f182956c783ec9e25ba
RemusCourtenay <rcou199@aucklanduni.ac.nz>
2022-09-26 10:57:37 +1300

    Added Task init

--- data/task.py
+++ data/task.py
@@ -1,8 +1,17 @@
+from typing import List
+
+
 class Task:
 
-    def __init__(self):
-        # TODO...
-        pass
+    def __init__(self,
+                 training_set: List,
+                 training_labels: List,
+                 validation_set: List,
+                 validation_labels: List):
+        self.training_set = training_set
+        self.training_labels = training_labels
+        self.validation_set = validation_set
+        self.validation_labels = validation_labels
 
 
 class TaskResult:
